f98f80c8a2fd5e21d70fa84cad90876a53b1853c
Your Name <you@example.com>
2025-10-20 18:27:10 +0800

    去重

--- backend/modules/common/controllers/ArticleCategoryController.php
+++ backend/modules/common/controllers/ArticleCategoryController.php
@@ -66,7 +66,7 @@ class ArticleCategoryController extends BaseController
             $dataProvider = $searchModel
                 ->search(Yii::$app->request->queryParams);
 //            $default_lang_model = Languages::find()->select(['code'])->where(['is_default' => 1])->one();
-            $default_lang = !empty($default_lang_model) ? $default_lang_model['code'] : "cn";
+            $default_lang = !empty($default_lang_model) ? $default_lang_model['code'] : "en";
             $lang = Yii::$app->request->get('lang', $default_lang);
 
             $dataProvider->query

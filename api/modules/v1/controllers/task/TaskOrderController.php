bb6bdff30bbd2e5c1e4ae01aff7da833fbb85e62
Your Name <you@example.com>
2025-11-06 19:34:43 +0800

    去重

--- api/modules/v1/controllers/task/TaskOrderController.php
+++ api/modules/v1/controllers/task/TaskOrderController.php
@@ -11,6 +11,7 @@ namespace api\modules\v1\controllers\task;
 
 use api\controllers\OnAuthController;
 use common\enums\StatusEnum;
+use common\helpers\ArrayHelper;
 use common\helpers\BcHelper;
 use common\helpers\DateHelper;
 use common\helpers\RedisHelper;
@@ -75,6 +76,20 @@ class TaskOrderController extends OnAuthController
     public function actionCreate()
     {
         RedisHelper::verify($this->memberId, $this->action->id);
+
+        // 判断特定任务
+//        $specific_task_settings = Yii::$app->debris->backendConfig('specific_task_settings');
+//        !empty($specific_task_settings) && $specific_task_settings = ArrayHelper::map(Json::decode($specific_task_settings), 'number', 'task_id');
+//        if (!empty($specific_task_settings)) {
+//            $order_count = Order::find()->where(['member_id' => $this->memberId])->count();
+//            foreach ($specific_task_settings as $k => $v) {
+//                // 如果已经领取的任务大于了领取特定任务数量，并且没有完成过特定任务
+//                if ($order_count >= $k && empty(Order::find()->where(['pid' => $v, 'member_id' => $this->memberId, 'status' => 2])->exists())) {
+//                    return ResultHelper::json(ResultHelper::ERROR_CODE, '必须完成特定任务才能继续领取下一个任务', ['task_id' => $v]);
+//                }
+//            }
+//        }
+
 
         $id = Yii::$app->request->post('id');
         $project = Project::find()->where(['id' => $id, 'status' => StatusEnum::ENABLED])->andWhere(['>', 'remain_number', 0])->one();

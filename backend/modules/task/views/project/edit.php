be86f00e43a9507de93bdfa5f0c8232efee9aa8e
Your Name <you@example.com>
2025-11-08 20:54:15 +0800

    sign

--- backend/modules/task/views/project/edit.php
+++ backend/modules/task/views/project/edit.php
@@ -108,7 +108,7 @@ $this->params['breadcrumbs'][] = $this->title;
                         ]
                     ]); ?>
                     <?= $form->field($model, 'keywords', ['options' => ['class' => ['chart']]])->textInput()->hint("*多个用“#”隔开", ['style' => 'color:red']) ?>
-                    <?= $form->field($model, 'push_content', ['options' => ['class' => ['chart']]])->textInput()->hint("*多个用“#”隔开", ['style' => 'color:red']) ?>
+                    <?= $form->field($model, 'push_content', ['options' => ['class' => ['chart']]])->textarea()->hint("*多个用“#”隔开", ['style' => 'color:red']) ?>
                     <?= $form->field($model, 'experience', ['options' => ['class' => ['chart']]])->textInput() ?>
 
 
